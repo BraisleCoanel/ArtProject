3d57e6b19eccd227e9331d42bc39a556a2dc238b
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-03-28 13:26:38 -0600

    Added imports into ShapeCanvas.

---
+++ src/art/view/ShapeCanvas.java
@@ -0,0 +1,24 @@
+package art.view;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+public class ShapeCanvas extends JPanel
+{
+
+}
