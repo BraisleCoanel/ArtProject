9fe7dedc7edd5d42e547ab6071c804b67ab137c6
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-04-10 13:14:53 -0600

    Created ArtFrame and ArtPanel

---
+++ src/art/view/ArtPanel.java
@@ -0,0 +1,15 @@
+package art.view;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import art.controller.ArtController;
+
+public class ArtPanel extends JPanel
+{
+	private final int MAXIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	
+}
